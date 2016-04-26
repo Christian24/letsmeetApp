c39c224197d888373b764e263894fcc4afbce8af
Jannik Wortmann <Jannik Wortmann>
2016-04-26 15:13:49 +0200

    Added Settings and Login Activity

--- app/src/main/java/com/webwemser/letsmeetapp/LoginActivity.java
+++ app/src/main/java/com/webwemser/letsmeetapp/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.webwemser.letsmeetapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,5 +17,15 @@ public class LoginActivity extends AppCompatActivity {
         //Notwendig damit der Hint den gleichen Font hat wie der Username
         EditText password = (EditText) findViewById(R.id.password_text);
         password.setTransformationMethod(new PasswordTransformationMethod());
+    }
+
+    public void login(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void signup(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/webwemser/letsmeetapp/MainActivity.java
+++ app/src/main/java/com/webwemser/letsmeetapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.webwemser.letsmeetapp;
 
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,12 +33,12 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
