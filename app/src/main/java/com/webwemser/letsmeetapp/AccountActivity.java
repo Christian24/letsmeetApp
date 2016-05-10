5c2bfb75539857eb2f83773fef334468336528b5
Jannik92 <jannikwortmann92@gmail.com>
2016-05-10 16:09:06 +0200

    Cleanup

--- app/src/main/java/com/webwemser/letsmeetapp/AccountActivity.java
+++ app/src/main/java/com/webwemser/letsmeetapp/AccountActivity.java
@@ -3,13 +3,22 @@ package com.webwemser.letsmeetapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private TextView username;
+    private EditText description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        //Initialize Views
+        username = (TextView)findViewById(R.id.accounnt_name);
+        description = (EditText)findViewById(R.id.account_description);
     }
 
     //Just closes the Activity at the moment, should later save account details
