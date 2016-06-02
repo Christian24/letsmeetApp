7fa49da290a00893609ed98e60b4981ebb3194b0
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-06-02 15:50:10 +0200

    Changed url, because url on server has changed

--- app/src/main/java/com/webwemser/web/KILOnlineIntegrationServiceSoapBinding.java
+++ app/src/main/java/com/webwemser/web/KILOnlineIntegrationServiceSoapBinding.java
@@ -35,7 +35,7 @@ public class KILOnlineIntegrationServiceSoapBinding
         java.lang.Object ProcessResult(KILExtendedSoapSerializationEnvelope __envelope,java.lang.Object result) throws java.lang.Exception;
     }
 
-    String url="http://10.0.2.2:8080/software-engeneering-wildfly-archetype-ejb/OnlineIntegration";
+    String url="http://10.0.2.2:8080/letsmeet/OnlineIntegration";
 
     int timeOut=60000;
     public List< HeaderProperty> httpHeaders;
