2588f78e6e53f9f43b021c8645d2f6756bf91355
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-06-16 19:59:51 +0200

    Updated MeetResponse

--- app/src/main/java/com/webwemser/web/MeetResponse.java
+++ app/src/main/java/com/webwemser/web/MeetResponse.java
@@ -15,50 +15,80 @@ import org.ksoap2.serialization.*;
 
 public class MeetResponse extends SessionResponse implements KvmSerializable
 {
+    public MeetData meet;
 
-
-    public MeetResponse()
+    public MeetResponse ()
     {
     }
 
-    public MeetResponse(Object paramObj, ExtendedSoapSerializationEnvelope __envelope)
+    public MeetResponse (java.lang.Object paramObj,ExtendedSoapSerializationEnvelope __envelope)
     {
-	    super(paramObj, __envelope);
-	    if (paramObj == null)
+        super(paramObj, __envelope);
+        if (paramObj == null)
             return;
         AttributeContainer inObj=(AttributeContainer)paramObj;
 
 
+        if(inObj instanceof SoapObject)
+        {
+            SoapObject soapObject=(SoapObject)inObj;
+            int size = soapObject.getPropertyCount();
+            for (int i0=0;i0< size;i0++)
+            {
+                //if you have compilation error here, please use a ksoap2.jar and ExKsoap2.jar from libs folder (in the generated zip file)
+                PropertyInfo info=soapObject.getPropertyInfo(i0);
+                java.lang.Object obj = info.getValue();
+                if (info.name.equals("meet"))
+                {
+                    if(obj!=null)
+                    {
+                        java.lang.Object j = obj;
+                        this.meet = (MeetData)__envelope.get(j,MeetData.class);
+                    }
+                    continue;
+                }
 
+            }
 
+        }
 
 
 
     }
 
     @Override
-    public Object getProperty(int propertyIndex) {
+    public java.lang.Object getProperty(int propertyIndex) {
         int count = super.getPropertyCount();
         //!!!!! If you have a compilation error here then you are using old version of ksoap2 library. Please upgrade to the latest version.
         //!!!!! You can find a correct version in Lib folder from generated zip file!!!!!
+        if(propertyIndex==count+0)
+        {
+            return this.meet!=null?this.meet:SoapPrimitive.NullSkip;
+        }
         return super.getProperty(propertyIndex);
     }
 
 
     @Override
     public int getPropertyCount() {
-        return super.getPropertyCount()+0;
+        return super.getPropertyCount()+1;
     }
 
     @Override
     public void getPropertyInfo(int propertyIndex, @SuppressWarnings("rawtypes") Hashtable arg1, PropertyInfo info)
     {
         int count = super.getPropertyCount();
+        if(propertyIndex==count+0)
+        {
+            info.type = MeetData.class;
+            info.name = "meet";
+            info.namespace= "";
+        }
         super.getPropertyInfo(propertyIndex,arg1,info);
     }
-    
+
     @Override
-    public void setProperty(int arg0, Object arg1)
+    public void setProperty(int arg0, java.lang.Object arg1)
     {
     }
 
