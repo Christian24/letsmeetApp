69cd0e62a27ee59b963837867841f190eac3ac41
Napfkuchen <sergeifladung@googlemail.com>
2016-06-19 07:36:09 +0200

    Added Login UI-Test

--- app/src/androidTest/java/com/webwemser/letsmeetapp/UITest.java
+++ app/src/androidTest/java/com/webwemser/letsmeetapp/UITest.java
@@ -13,20 +13,27 @@ import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 
 /**
- * 
+ * Testclass in order to test Userinteractions with application
  * @author Sergei Fladung
  */
 @RunWith(AndroidJUnit4.class)
-public class UITest {
-	
-	private String name = "Günther Gans";
-	
-	 @Rule
-	 public ActivityTestRule<MainActivity> testingActivity = new ActivityTestRule<>(MainActivity.class);
-	 
-	 @Test
-	 public void testSomething() {
-		 
-	 }
-	
+class UITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> testingActivity = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * Testuses should be successfull logged in.
+     */
+    @Test
+    public void loginTest() {
+        String username = "Charlotte";
+        String password = "Webwemser";
+
+        onView(withId(R.id.username_text)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.password_text)).perform(typeText(password), closeSoftKeyboard());
+
+        onView(withId(R.id.login_button)).perform(click());
+    }
+
 }
